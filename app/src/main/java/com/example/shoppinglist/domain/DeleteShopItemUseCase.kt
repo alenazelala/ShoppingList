44796d802942b59ae0e640e2f94f2ff70818e1b7
alenazelala <143320414+alenazelala@users.noreply.github.com>
2024-02-29 14:46:06 +0300

    Create UseCase

---
+++ app/src/main/java/com/example/shoppinglist/domain/DeleteShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.shoppinglist.domain
+
+class DeleteShopItemUseCase {
+
+    fun deleteItem(shopItem: ShopItem){
+
+    }
+}
---
+++ app/src/main/java/com/example/shoppinglist/domain/UpdateItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.shoppinglist.domain
+
+class UpdateItemUseCase {
+
+    fun UpdateItem(shopItem: ShopItem){
+       TODO()
+    }
+}
