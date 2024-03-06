129c720eff4d2864100006065c93d8dcdd70c91b
alenazelala <143320414+alenazelala@users.noreply.github.com>
2024-03-06 11:48:41 +0300

    Create viewModel

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,21 +1,28 @@
 package com.example.shoppinglist.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.shoppinglist.domain.ShopItem
 import com.example.shoppinglist.domain.ShopListRepository
 import java.lang.RuntimeException
 
 object ShopListRepositoryImpl: ShopListRepository {
+
+    private val shopListld = MutableLiveData<List<ShopItem>>()
     private val shopList = mutableListOf<ShopItem>()
     private var autoIncrementId = 0
 
     override fun addShopItem(shopItem: ShopItem) {
-        if(shopItem.id == ShopItem.UNDEFIND_ID){
-        shopItem.id = autoIncrementId++}
+        if (shopItem.id == ShopItem.UNDEFIND_ID) {
+            shopItem.id = autoIncrementId++
+        }
         shopList.add(shopItem)
+        updateList()
     }
 
     override fun deleteItem(shopItem: ShopItem) {
-       shopList.remove(shopItem)
+        shopList.remove(shopItem)
+        updateList()
     }
 
     override fun updateItem(shopItem: ShopItem) {
@@ -25,11 +32,14 @@ object ShopListRepositoryImpl: ShopListRepository {
     }
 
     override fun getShopItem(shopItemId: Int): ShopItem {
-        return shopList.find { it.id == shopItemId}?:
-        throw RuntimeException("Element with id $shopItemId not found")
+        return shopList.find { it.id == shopItemId }
+            ?: throw RuntimeException("Element with id $shopItemId not found")
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toList()
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListld
+    }
+    private fun updateList(){
+        shopListld.value = shopList.toList()
     }
 }
--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel : MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this){
+
+        }
     }
 }
